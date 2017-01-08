6bae85c99698e34e7480346213c63b4412b45d0e
Botond Fekete <fekbot@gmail.com>
2017-01-08 19:29:20 +0000

    Removed generated certificates as going to stick with http1.1 for now

--- api/dist/server/index.js
+++ api/dist/server/index.js
@@ -5,5 +5,5 @@ const adapters_1 = require("./adapters");
 const middlewares_1 = require("./middlewares");
 const routes_1 = require("./routes");
 const start = (msg) => ramda_1.compose(ramda_1.flip(ramda_1.call)(msg), ramda_1.prop('start'));
-exports.api = config => ramda_1.compose(ramda_1.tap(start('Server listening')), ramda_1.tap(routes_1.addRoutes(routes_1.routes)), ramda_1.tap(middlewares_1.addMiddlewares(config, middlewares_1.middlewares)), adapters_1.adapter(restify_1.createServer), ramda_1.tap(console.log))(config);
+exports.api = config => ramda_1.compose(ramda_1.tap(start('Server listening')), ramda_1.tap(routes_1.addRoutes(routes_1.routes)), ramda_1.tap(middlewares_1.addMiddlewares(config, middlewares_1.middlewares)), adapters_1.adapter(restify_1.createServer))(config);
 //# sourceMappingURL=index.js.map
