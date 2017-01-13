e1b44e0e91cc2e4d60dd02676b71968f8757020c
Botond Fekete <fekbot@gmail.com>
2017-01-13 21:17:19 +0000

    Added dummy root path handler

--- api/dist/server/routes/index.js
+++ api/dist/server/routes/index.js
@@ -5,6 +5,7 @@ const prezis_1 = require("./prezis");
 const services_1 = require("../../services");
 const add_1 = require("./add");
 const specs = [
+    () => ({ path: '/', handlers: { get: (res, req) => res.send(200, "HI THERE!") } }),
     create_1.create('prezis', prezis_1.prezis),
 ];
 exports.routes = ramda_1.compose(add_1.addServices(specs), services_1.services);
--- api/src/server/routes/index.ts
+++ api/src/server/routes/index.ts
@@ -17,6 +17,7 @@ import {services} from '../../services'
 import {addServices} from './add'
 
 const specs: RouteHandlers[] = [
+  () => ({path: '/', handlers: { get: (res, req) => res.send(200, "HI THERE!")}}),
   create('prezis', prezis),
 ]
 
